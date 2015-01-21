f7eb3bff35df5d11b347aa1f5d829ab7607d52c1
bekutaa <erika.auyeung@sbcglobal.net>
2015-01-21 10:30:32 -0800

    Programmed tree.

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,19 +1,37 @@
-private double fractionLength = 0.8;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = 0.8f;
 private int smallestBranch = 8;
-private double branchAngle = 0.5;
+private double branchAngle = 0.5f;
 private float bLength = 10;
 private int bThick = 8;
 
 public void setup()
 {
 	size(640,480);
+	// frameRate(600);
+//	noLoop();
 }
 
 public void draw()
 {
 	background(0);
 	stroke(0,255,0);
-	strokeWeight(bThick/0.9);
+	strokeWeight(bThick/0.9f);
 	line(320,480,320,380);
 	drawBranches(320,380,bLength,3*Math.PI/2,bThick);
 
@@ -29,7 +47,7 @@ public void drawBranches(float x, float y, double branchLength, double angle, in
 
 	branchLength = branchLength*fractionLength;
 
-	int drawWidth = (int)(thickness*0.9);
+	int drawWidth = (int)(thickness*0.9f);
 	if(drawWidth < 1)
 	{
 		drawWidth = 1;
@@ -51,4 +69,13 @@ public void drawBranches(float x, float y, double branchLength, double angle, in
 		drawBranches(endX1,endY1,branchLength,angle1+branchAngle/2,drawWidth);
 		drawBranches(endX2,endY2,branchLength,angle2-branchAngle/2,drawWidth);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
